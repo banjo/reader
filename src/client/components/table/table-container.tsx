84bef398240ff76786e1fba882942a4fa72476c2
Anton Ödman <anton.odman@gmail.com>
2023-06-07 22:03:30 +0200

    chore: switch for filter

--- src/client/components/table/table-container.tsx
+++ src/client/components/table/table-container.tsx
@@ -3,9 +3,10 @@
 import { MenuEntries } from "@/client/components/shared/Dropdown";
 import { Table } from "@/client/components/table/table";
 import { TableItem } from "@/client/components/table/table-item";
+import { Switch } from "@/client/components/ui/switch";
 import { CleanFeedWithItems, CleanItem } from "@/shared/models/entities";
 import { Refetch } from "@/shared/models/swr";
-import { FC, useMemo } from "react";
+import { FC, useMemo, useState } from "react";
 
 type TableContainerProps = {
     feeds: CleanFeedWithItems[];
@@ -13,44 +14,92 @@ type TableContainerProps = {
     refetch: Refetch<CleanItem>;
 };
 
-type FormattedFeed = {
-    id: number;
-    name: string;
-    items: CleanItem[];
+type TableCard = CleanItem & {
+    feedName: string;
+};
+
+type TableFilters = {
+    showUnreadOnly: boolean;
+    toggleShowUnreadOnly: () => void;
+};
+
+type TableFiltersOut = {
+    filters: TableFilters;
+    data: TableCard[];
+};
+
+const useTableFilters = (data: TableCard[]): TableFiltersOut => {
+    const [showUnreadOnly, setShowUnreadOnly] = useState<boolean>(true);
+
+    const filteredData = useMemo(() => {
+        if (showUnreadOnly) {
+            return data.filter(item => item.isRead === false);
+        }
+        return data;
+    }, [data, showUnreadOnly]);
+
+    const toggleShowUnreadOnly = () => {
+        setShowUnreadOnly(prev => !prev);
+    };
+
+    return { filters: { showUnreadOnly, toggleShowUnreadOnly }, data: filteredData };
+};
+
+type FilterBarProps = {
+    filters: TableFilters;
+};
+
+export const FilterBar: FC<FilterBarProps> = ({ filters }) => {
+    const { showUnreadOnly, toggleShowUnreadOnly } = filters;
+    return (
+        <div className="flex h-16 w-full items-center justify-end rounded-md bg-slate-100 p-4">
+            <div className="flex items-center">
+                <Switch
+                    id="show-unread"
+                    checked={showUnreadOnly}
+                    onCheckedChange={() => toggleShowUnreadOnly()}
+                />
+                <label htmlFor="show-unread" className="ml-2 text-sm font-medium">
+                    Unread
+                </label>
+            </div>
+        </div>
+    );
 };
 
 export const TableContainer: FC<TableContainerProps> = ({ feeds, menuOptions, refetch }) => {
     const multipleFeeds = useMemo(() => feeds.length > 1, [feeds]);
 
-    const data: FormattedFeed[] = useMemo(
-        () =>
-            feeds.map(feed => {
-                return {
-                    id: feed.id,
-                    name: feed.name,
-                    items: feed.items,
-                };
-            }),
-        [feeds]
-    );
+    const formattedData: TableCard[] = useMemo(() => {
+        const tableCards: TableCard[] = [];
+        for (const feed of feeds) {
+            for (const item of feed.items) {
+                tableCards.push({ ...item, feedName: feed.name });
+            }
+        }
+        return tableCards;
+    }, [feeds]);
+
+    const { data, filters } = useTableFilters(formattedData);
 
     return (
-        <Table type="list">
-            {data.map(formattedFeed => {
-                return formattedFeed.items.map(item => {
+        <>
+            <FilterBar filters={filters} />
+            <Table type="list">
+                {data.map(item => {
                     return (
                         <TableItem
                             key={item.id}
                             item={item}
                             type="list"
-                            feedName={formattedFeed.name}
+                            feedName={item.feedName}
                             showFeedName={multipleFeeds}
                             menuOptions={menuOptions}
                             refetch={refetch}
                         />
                     );
-                });
-            })}
-        </Table>
+                })}
+            </Table>
+        </>
     );
 };
