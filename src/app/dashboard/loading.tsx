7dfef11d3db0d3ad8ea95b788e13e6a42a6a684b
Anton Ödman <anton.odman@gmail.com>
2023-06-08 22:15:05 +0200

    chore: update skeleton

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -3,7 +3,8 @@ import { range } from "@banjoanton/utils";
 
 export default function Loading() {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-8">
+            <Skeleton className="h-32 w-full rounded-md" />
             {range(20).map(i => (
                 <Skeleton key={i} className="h-8 w-full rounded-md" />
             ))}
