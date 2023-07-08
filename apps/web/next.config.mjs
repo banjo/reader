97266e0016a9eb2083fffe894b658d3df072e963
Anton Ödman <anton.odman@gmail.com>
2023-07-08 22:50:13 +0200

    chore: config and package updates

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
     webpack(config) {
         config.experiments = { ...config.experiments, topLevelAwait: true };
         return config;
     },
+    transpilePackages: ["db"],
 };
 
 export default nextConfig;
