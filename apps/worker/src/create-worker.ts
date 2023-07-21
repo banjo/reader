d290bbec289782bc3ba1c5d72c104df4d0787ce9
Anton Ödman <anton.odman@gmail.com>
2023-07-21 17:06:24 +0200

    chore: add logs to deploy

--- apps/worker/src/create-worker.ts
+++ apps/worker/src/create-worker.ts
@@ -1,13 +1,15 @@
 import { options } from "@/config";
 import { toMilliseconds } from "@banjoanton/utils";
 import { Job, Queue, Worker } from "bullmq";
-import { paramCase } from "change-case";
-import { ResultType } from "server";
+import { paramCase, pascalCase } from "change-case";
+import { ResultType, createLogger } from "server";
+
 
 export const createWorker = <T extends object>(
     name: string,
     processor: (job: Job<T>) => Promise<ResultType<void>>
 ) => {
+    const logger = createLogger(`CreateWorker${pascalCase(name)}`);
     const QUEUE_NAME = `${paramCase(name)}-queue`;
     const JOB_NAME = `${paramCase(name)}-job`;
     const queue = new Queue<T>(QUEUE_NAME, options);
@@ -15,16 +17,20 @@ export const createWorker = <T extends object>(
 
     const wrapper = {
         start: async () => {
+            logger.info("Starting...");
             await worker.waitUntilReady();
         },
         add: async (data: T) => {
+            logger.info("Adding job...");
             await queue.add(JOB_NAME, data);
         },
         close: async () => {
+            logger.info("Closing...");
             await queue.close();
             await worker.close();
         },
         repeatable: async (data: T, timeInMs = toMilliseconds({ minutes: 15 })) => {
+            logger.info(`Adding repeatable job every ${timeInMs}ms...`);
             await queue.add(JOB_NAME, data, {
                 repeat: {
                     every: timeInMs,
@@ -34,6 +40,7 @@ export const createWorker = <T extends object>(
         },
         stopRepeatable: async () => {
             const repeatableJobs = await queue.getRepeatableJobs();
+            logger.info(`Stopping ${repeatableJobs.length} repeatable jobs...`);
             await Promise.all(
                 repeatableJobs.map(async job => {
                     await queue.removeRepeatableByKey(job.key);
@@ -41,6 +48,7 @@ export const createWorker = <T extends object>(
             );
         },
         activeCount: async () => {
+            logger.info("Getting active count...");
             return await queue.getActiveCount();
         },
     };
