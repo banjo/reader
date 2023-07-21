f599ab14d32ea9fefbb98b63fbb43534e57ef6ed
Anton Ödman <anton.odman@gmail.com>
2023-07-21 17:10:05 +0200

    chore: add logs to add

--- apps/worker/src/add.ts
+++ apps/worker/src/add.ts
@@ -1,14 +1,23 @@
 import { fetchWorker } from "@/workers/fetch/fetch-worker";
 import { FeedWithContent, prisma } from "db";
+import { createLogger } from "server";
+
+const logger = createLogger("AddFetchRss");
 
 export const addFetchRss = async () => {
+    logger.info("Adding fetch rss...");
     const feeds: FeedWithContent[] = await prisma.feed.findMany({
         include: {
             contentItems: true,
         },
     });
 
+    logger.info(`Found ${feeds.length} feeds`);
+
     for (const feed of feeds) {
+        logger.info(`Adding ${feed.url} to fetch rss...`);
         await fetchWorker.repeatable({ feed });
     }
+
+    logger.info("Done");
 };
