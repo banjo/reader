dadeb14a4058957f8f8fca738407288ce295ed68
Anton Ödman <anton.odman@gmail.com>
2023-07-21 16:01:56 +0200

    chore: add password for redis

--- apps/worker/src/config.ts
+++ apps/worker/src/config.ts
@@ -2,10 +2,12 @@ import { QueueOptions } from "bullmq";
 
 const HOST = process.env.REDIS_HOST ?? "localhost";
 const PORT = parseInt(process.env.REDIS_PORT ?? "6379");
+const PASSWORD = process.env.REDIS_PASSWORD ?? undefined;
 
 export const options: QueueOptions = {
     connection: {
         host: HOST,
         port: PORT,
+        password: PASSWORD,
     },
 };
