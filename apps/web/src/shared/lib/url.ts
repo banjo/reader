cf88b799c970cee8428919437ed34ac8020ee3d1
Anton Ödman <anton.odman@gmail.com>
2023-07-23 22:12:37 +0200

    chore: update prod url

--- apps/web/src/shared/lib/url.ts
+++ apps/web/src/shared/lib/url.ts
@@ -1,5 +1,5 @@
 export const getUrl = () => {
     return process.env.NODE_ENV === "development"
         ? "http://localhost:3002"
-        : "https://banjo-rss-web.vercel.app";
+        : "https://banjo-rss-web.host.banjoanton.com";
 };
