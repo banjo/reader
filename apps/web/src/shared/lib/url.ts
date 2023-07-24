c4221edf9380cb5413c9449b592d724288da78ee
Anton Ödman <anton.odman@gmail.com>
2023-07-24 23:47:50 +0200

    chore: add prod url to web

--- apps/web/src/shared/lib/url.ts
+++ apps/web/src/shared/lib/url.ts
@@ -1,5 +1,5 @@
+const PROD_URL = process.env.WEB_PROD_URL;
+
 export const getUrl = () => {
-    return process.env.NODE_ENV === "development"
-        ? "http://localhost:3002"
-        : "https://banjo-rss-web.host.banjoanton.com";
+    return process.env.NODE_ENV === "development" ? "http://localhost:3002" : PROD_URL;
 };
