6691598d21f6a3d7eed09a6892617c1fb1581fce
Anton Ödman <anton.odman@gmail.com>
2023-07-24 23:56:10 +0200

    test: debug log

--- apps/web/src/shared/lib/url.ts
+++ apps/web/src/shared/lib/url.ts
@@ -1,5 +1,8 @@
 const PROD_URL = process.env.WEB_PROD_URL;
 
 export const getUrl = () => {
+    console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+    console.log("PROD_URL", PROD_URL);
+
     return process.env.NODE_ENV === "development" ? "http://localhost:3002" : PROD_URL;
 };
