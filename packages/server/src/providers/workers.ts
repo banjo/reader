61a146ec26f389df74648cdfa095c72c59b3ad32
Anton Ödman <anton.odman@gmail.com>
2023-07-25 14:52:30 +0200

    chore: use fetch instead of ky

--- packages/server/src/providers/workers.ts
+++ packages/server/src/providers/workers.ts
@@ -1,23 +1,14 @@
 import { raise } from "@banjoanton/utils";
-import ky from "ky";
-import { Result } from "utils";
+import { Result, createLogger } from "utils";
 
 const PROD_URL = process.env.WORKER_PROD_URL ?? raise("Missing WORKER_PROD_URL");
 const AUTH_TOKEN = process.env.AUTH_TOKEN ?? raise("Missing AUTH_TOKEN");
 const url = process.env.NODE_ENV === "production" ? PROD_URL : "http://localhost:3000";
 
-const api = ky.create({
-    prefixUrl: `${url}/api`,
-    headers: {
-        "auth-token": AUTH_TOKEN,
-        "Content-Type": "application/json",
-    },
-});
+const logger = createLogger("WorkerProvider");
 
 export const addRepeatableJob = async (feedId: number) => {
     try {
-        console.log(`Adding to URL: ${url}`);
-        // await api.get(`repeatable?feedId=${feedId}`, {}).json<ResultType<void>>();
         await fetch(`${url}/api/repeatable?feedId=${feedId}`, {
             headers: {
                 "auth-token": AUTH_TOKEN,
@@ -25,13 +16,13 @@ export const addRepeatableJob = async (feedId: number) => {
         });
         return Result.okEmpty();
     } catch (error: unknown) {
-        console.log("Could not add repeatable job");
+        logger.error("Could not add repeatable job");
         let errorMessage = "Internal Server Error";
         if (error instanceof Error) {
-            console.log(error.message);
+            logger.error(error.message);
             errorMessage = error.message;
         } else if (typeof error === "string") {
-            console.log(error);
+            console.error(error);
             errorMessage = error;
         }
 
