51409da2506b311f1a3aed795f07c977e48958a3
Anton Ödman <anton.odman@gmail.com>
2023-07-25 10:21:39 +0200

    chore: test prod url directly

--- apps/web/src/shared/lib/url.ts
+++ apps/web/src/shared/lib/url.ts
@@ -1,8 +1,7 @@
-const PROD_URL = process.env.WEB_PROD_URL;
+// const PROD_URL = process.env.WEB_PROD_URL;
 
 export const getUrl = () => {
-    console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-    console.log("PROD_URL", PROD_URL);
-
-    return process.env.NODE_ENV === "development" ? "http://localhost:3002" : PROD_URL;
+    return process.env.NODE_ENV === "development"
+        ? "http://localhost:3002"
+        : "https://banjo-rss-web.vercel.app";
 };
