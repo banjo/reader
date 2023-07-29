c20bf540b6638a2267a4a5423366c0e48dcb072c
Anton Ödman <anton.odman@gmail.com>
2023-07-29 13:52:49 +0200

    chore: build dockerfile for web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    server: {
+        host: "0.0.0.0",
+        port: 3000,
+    },
 });
