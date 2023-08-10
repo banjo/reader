cc202cfee8370d3acd68647d295b2448a3516be1
Anton Ödman <anton.odman@gmail.com>
2023-08-10 20:36:10 +0200

    chore: fix all container

--- apps/web/src/features/items/containers/all-container.tsx
+++ apps/web/src/features/items/containers/all-container.tsx
@@ -1,25 +1,18 @@
+import { TableSkeleton } from "@/components/shared/table-skeleton";
 import { TableContainerItems } from "@/components/table/table-container-items";
 import { useItemsFetcher } from "@/features/items/hooks/use-items-fetcher";
 import { useTableItemMenu } from "@/hooks/shared/use-table-item-menu";
-import { noop } from "@banjoanton/utils";
 
 export const AllContainer = () => {
-    const { data, refetch } = useItemsFetcher({
-        key: "/items",
-    });
-    const { menuOptionsItems } = useTableItemMenu({
-        refetchItemsMultiple: refetch,
-        refetchContentMultiple: noop,
-    });
+    const { data, isLoading } = useItemsFetcher();
+    const { menuOptionsItems } = useTableItemMenu();
+
+    if (isLoading) return <TableSkeleton />;
+    if (!data) return null;
 
     return (
         <div className="flex flex-col gap-4">
-            <TableContainerItems
-                items={data}
-                menuOptions={menuOptionsItems}
-                refetch={refetch}
-                title={"All"}
-            />
+            <TableContainerItems items={data} menuOptions={menuOptionsItems} title={"All"} />
         </div>
     );
 };
--- apps/web/src/features/items/hooks/use-items-fetcher.ts
+++ apps/web/src/features/items/hooks/use-items-fetcher.ts
@@ -1,58 +1,18 @@
 import { useAuthFetcher } from "@/hooks/backend/use-auth-fetcher";
-import { useUpdateSidebar } from "@/hooks/backend/use-update-sidebar";
-import { Refetch } from "@/models/swr";
+import { toMilliseconds } from "@banjoanton/utils";
+import { useQuery } from "@tanstack/react-query";
 import { ItemWithContent } from "db";
-import { useMemo } from "react";
-import useSWR from "swr";
 
-type Out = {
-    data: ItemWithContent[];
-    isLoading: boolean;
-    refetch: Refetch<ItemWithContent[]>;
-};
+export const useItemsFetcher = () => {
+    const { QUERY: fetcher } = useAuthFetcher();
 
-type In = {
-    key: string;
-};
+    const { data, isLoading } = useQuery<ItemWithContent[]>({
+        queryKey: ["items"],
+        queryFn: async () => await fetcher(`/items`),
+        staleTime: toMilliseconds({ hours: 1 }),
+        initialData: [],
+        initialDataUpdatedAt: 0,
+    });
 
-export const useItemsFetcher = ({ key }: In): Out => {
-    const { SWR_AUTH: fetcher } = useAuthFetcher();
-    const { fetchLatestInSidebar, mutateSidebarItems } = useUpdateSidebar();
-
-    const { data: fetchData, mutate } = useSWR<ItemWithContent[], Error>(key, fetcher, {});
-
-    const data = useMemo(() => {
-        return fetchData ?? [];
-    }, [fetchData]);
-
-    const refetch: Refetch<ItemWithContent[]> = async (updatedItems, updateFn, onError) => {
-        const allItems = data.map(i => {
-            const updatedItem = updatedItems.find(ui => ui.id === i.id);
-
-            if (updatedItem) {
-                return updatedItem;
-            }
-
-            return i;
-        });
-
-        mutate(allItems, false);
-        mutateSidebarItems(updatedItems);
-
-        try {
-            await updateFn();
-        } catch (error) {
-            console.error(error);
-            if (onError) onError();
-        }
-
-        mutate();
-        fetchLatestInSidebar();
-    };
-
-    return {
-        data,
-        isLoading: !data,
-        refetch,
-    };
+    return { data, isLoading };
 };
