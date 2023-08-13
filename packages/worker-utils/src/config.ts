d401f380893616291f9fc0f3f256513deb5eb458
Anton Ödman <anton.odman@gmail.com>
2023-08-13 15:17:47 +0200

    chore: change to use local worker in dev

--- packages/worker-utils/src/config.ts
+++ packages/worker-utils/src/config.ts
@@ -1,13 +1,24 @@
+import { raise } from "@banjoanton/utils";
 import { QueueOptions } from "bullmq";
 
-const HOST = process.env.REDIS_HOST ?? "localhost";
-const PORT = Number.parseInt(process.env.REDIS_PORT ?? "6379");
-const PASSWORD = process.env.REDIS_PASSWORD ?? undefined;
+const getConnection = () => {
+    const isProd = process.env.NODE_ENV === "production";
+
+    if (isProd) {
+        return {
+            host: process.env.REDIS_HOST ?? raise("REDIS_HOST is not set"),
+            port: Number.parseInt(process.env.REDIS_PORT ?? raise("REDIS_PORT is not set")),
+            password: process.env.REDIS_PASSWORD ?? raise("REDIS_PASSWORD is not set"),
+        };
+    }
+
+    return {
+        host: "localhost",
+        port: 6379,
+        password: undefined,
+    };
+};
 
 export const redisConfig: QueueOptions = {
-    connection: {
-        host: HOST,
-        port: PORT,
-        password: PASSWORD,
-    },
+    connection: getConnection(),
 };
