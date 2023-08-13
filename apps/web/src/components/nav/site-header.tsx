d4ef50d25129b8351a61e898d52d6979f25468e2
Anton Ödman <anton.odman@gmail.com>
2023-08-13 22:25:13 +0200

    chore: only show menu bar when signed in

--- apps/web/src/components/nav/site-header.tsx
+++ apps/web/src/components/nav/site-header.tsx
@@ -1,5 +1,6 @@
 import { MainNav } from "@/components/nav/main-nav";
 import { siteConfig } from "@/config/site";
+import { useAuth } from "@/hooks/backend/use-auth";
 import { useMenuStore } from "@/stores/useMenuStore";
 import { UserButton } from "@clerk/clerk-react";
 import Hamburger from "hamburger-react";
@@ -7,6 +8,7 @@ import Hamburger from "hamburger-react";
 export function SiteHeader() {
     const isOpen = useMenuStore(state => state.isOpen);
     const toggleMenu = useMenuStore(state => state.toggle);
+    const { userId } = useAuth();
 
     return (
         <header className="top-0 z-40 w-full border-b bg-background">
@@ -18,9 +20,11 @@ export function SiteHeader() {
                             <UserButton afterSignOutUrl="/" />
                         </div>
 
-                        <div className="pl-4 absolute left-5 z-50 md:hidden">
-                            <Hamburger onToggle={toggleMenu} toggled={isOpen} />
-                        </div>
+                        {userId && (
+                            <div className="pl-4 absolute left-5 z-50 md:hidden">
+                                <Hamburger onToggle={toggleMenu} toggled={isOpen} />
+                            </div>
+                        )}
                     </nav>
                 </div>
             </div>
