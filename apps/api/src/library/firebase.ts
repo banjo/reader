5f8355c0a0a804a6a9bb416c405d1adba3da961d
Anton Ödman <anton.odman@gmail.com>
2023-08-22 20:23:45 +0200

    chore: use env variables instead of file for firebase server

--- apps/api/src/library/firebase.ts
+++ apps/api/src/library/firebase.ts
@@ -1,9 +1,21 @@
-import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { raise } from "@banjoanton/utils";
+import { cert, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
+import "dotenv/config";
+
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID ?? raise("FIREBASE_PROJECT_ID missing"),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ?? raise("FIREBASE_PRIVATE_KEY missing"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? raise("FIREBASE_CLIENT_EMAIL missing"),
+};
+
 const app = initializeApp({
-    credential: applicationDefault(),
-    projectId: "banjo-rss",
+    credential: cert({
+        projectId: serviceAccount.projectId,
+        privateKey: serviceAccount.privateKey.replace(/\\n/g, "\n"),
+        clientEmail: serviceAccount.clientEmail,
+    }),
 });
 
 export const auth = getAuth(app);
