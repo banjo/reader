dd690ab53bffffa9fbf193cd374d670cf688d1b6
Anton Ödman <anton.odman@gmail.com>
2023-08-31 10:36:42 +0200

    Revert "chore: ugly solution for import"
    This reverts commit de46a6a1d1263cc6010a8c46d92aca0a1aa3af7f.

--- apps/web/src/hooks/backend/use-add-feed.ts
+++ apps/web/src/hooks/backend/use-add-feed.ts
@@ -1,5 +1,4 @@
 import { useAuthFetcher } from "@/hooks/backend/use-auth-fetcher";
-import { Result } from "@/lib/result";
 
 export type FeedAddManyReturnType = {
     total: number;
@@ -14,28 +13,8 @@ export type FeedAddManyReturnType = {
 export const useAddFeed = () => {
     const api = useAuthFetcher();
 
-    const addMany = async (urls: string[]) => {
-        // this solution does not work because the backend crashes on Promise.allSettled
-        // await api.POST<FeedAddManyReturnType>(`/feed/add-many`, { urls });
+    const addMany = async (urls: string[]) =>
+        await api.POST<FeedAddManyReturnType>(`/feed/add-many`, { urls });
 
-        const results = await Promise.allSettled(urls.map(url => api.POST(`/feed`, { url })));
-
-        const total = results.length;
-        const success = results.filter(result => result.status === "fulfilled").length;
-
-        const errors = results
-            .filter(result => result.status === "rejected")
-            .map(r => {
-                const result = r as PromiseRejectedResult;
-
-                return {
-                    url: result.reason.url,
-                    message: result.reason.message,
-                    type: result.reason.type,
-                };
-            });
-
-        return Result.ok({ total, success, errors });
-    };
     return { addMany };
 };
