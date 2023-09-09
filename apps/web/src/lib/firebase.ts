a437d919a2f307c5630a1604dc34c6cf3a83e1db
Anton Ödman <anton.odman@gmail.com>
2023-09-09 13:53:55 +0200

    chore: update firebase for tests

--- apps/web/src/lib/firebase.ts
+++ apps/web/src/lib/firebase.ts
@@ -1,23 +1,32 @@
-import { raise } from "@banjoanton/utils";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? raise("VITE_FIREBASE_API_KEY missing"),
+//     authDomain:
+//         import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? raise("VITE_FIREBASE_AUTH_DOMAIN missing"),
+//     projectId:
+//         import.meta.env.VITE_FIREBASE_PROJECT_ID ?? raise("VITE_FIREBASE_PROJECT_ID missing"),
+//     storageBucket:
+//         import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ??
+//         raise("VITE_FIREBASE_STORAGE_BUCKET missing"),
+//     messagingSenderId:
+//         import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ??
+//         raise("VITE_FIREBASE_MESSAGING_SENDER_ID missing"),
+//     appId: import.meta.env.VITE_FIREBASE_APP_ID ?? raise("VITE_FIREBASE_APP_ID missing"),
+//     measurementId:
+//         import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ??
+//         raise("VITE_FIREBASE_MEASUREMENT_ID missing"),
+// };
+
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? raise("VITE_FIREBASE_API_KEY missing"),
-    authDomain:
-        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? raise("VITE_FIREBASE_AUTH_DOMAIN missing"),
-    projectId:
-        import.meta.env.VITE_FIREBASE_PROJECT_ID ?? raise("VITE_FIREBASE_PROJECT_ID missing"),
-    storageBucket:
-        import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ??
-        raise("VITE_FIREBASE_STORAGE_BUCKET missing"),
-    messagingSenderId:
-        import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ??
-        raise("VITE_FIREBASE_MESSAGING_SENDER_ID missing"),
-    appId: import.meta.env.VITE_FIREBASE_APP_ID ?? raise("VITE_FIREBASE_APP_ID missing"),
-    measurementId:
-        import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ??
-        raise("VITE_FIREBASE_MEASUREMENT_ID missing"),
+    apiKey: "AIzaSyBwINPEigXro7wlkGCtVYOBbKgTEzGu2GE",
+    authDomain: "banjo-rss-auth.firebaseapp.com",
+    projectId: "banjo-rss-auth",
+    storageBucket: "banjo-rss-auth.appspot.com",
+    messagingSenderId: "15126327526",
+    appId: "1:15126327526:web:38f4a1c045053b26a04b27",
+    measurementId: "G-ZJ3JLPEVE8",
 };
 
 export const app = initializeApp(firebaseConfig);
--- packages/firebase-server/src/lib/firebase.ts
+++ packages/firebase-server/src/lib/firebase.ts
@@ -5,16 +5,11 @@ import { getAuth } from "firebase-admin/auth";
 import "dotenv/config";
 
 const serviceAccount = {
-    // projectId: process.env.FIREBASE_PROJECT_ID ?? raise("FIREBASE_PROJECT_ID missing"),
-    // privateKey: process.env.FIREBASE_PRIVATE_KEY ?? raise("FIREBASE_PRIVATE_KEY missing"),
-    // clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? raise("FIREBASE_CLIENT_EMAIL missing"),
     key: process.env.FIREBASE_ADMIN_KEY ?? raise("FIREBASE_ADMIN_KEY missing"),
 };
 
 const firebaseBuffer = Buffer.from(serviceAccount.key, "base64");
 const firebaseKey = firebaseBuffer.toString("utf8");
-
-console.log("🪕%c Banjo | firebase.ts:17 |", "color: #E91E63", firebaseKey);
 
 const app = initializeApp({
     credential: cert(JSON.parse(firebaseKey)),
