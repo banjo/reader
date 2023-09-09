6ee1c4c5374bc8120c45a9c9fba01ff0b7a04de5
Anton Ödman <anton.odman@gmail.com>
2023-09-09 10:47:00 +0200

    chore: remove old firebase server config

--- packages/firebase-server/src/lib/firebase.ts
+++ packages/firebase-server/src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getAuth } from "firebase-admin/auth";
 import "dotenv/config";
 
 const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID ?? raise("FIREBASE_PROJECT_ID missing"),
-    privateKey: process.env.FIREBASE_PRIVATE_KEY ?? raise("FIREBASE_PRIVATE_KEY missing"),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? raise("FIREBASE_CLIENT_EMAIL missing"),
+    // projectId: process.env.FIREBASE_PROJECT_ID ?? raise("FIREBASE_PROJECT_ID missing"),
+    // privateKey: process.env.FIREBASE_PRIVATE_KEY ?? raise("FIREBASE_PRIVATE_KEY missing"),
+    // clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? raise("FIREBASE_CLIENT_EMAIL missing"),
     key: process.env.FIREBASE_ADMIN_KEY ?? raise("FIREBASE_ADMIN_KEY missing"),
 };
 
